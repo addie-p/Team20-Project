8e59237e4327922dfd9939fe84432f375237ffa9
Addie Padhi <aaditipadhi@umass.edu>
2024-11-14 17:19:54 -0500

    Revert Commit (Wrong branch)
    This reverts commit f2e16b4e16a61773f9cfb5f58a689282b2712b01.

--- team/front-end/source/main.js
+++
@@ -1,6 +0,0 @@
-import { NavBarComponent } from './components/NavBarComponent/navbar.js';
-
-const app = document.getElementById('app');
-
-const navBar = new NavBarComponent();
-app.appendChild(navBar.render());
